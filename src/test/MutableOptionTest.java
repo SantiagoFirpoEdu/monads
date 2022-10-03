f78042a419236583ab6573fa6c2a269c8e3dac72
Santiago Firpo <santiago.firpo@aquiris.com.br>
2022-10-03 16:51:20 -0300

    Renamed tests to test and added JUnit as dependency

--- src/test/MutableOptionTest.java
+++ src/test/MutableOptionTest.java
@@ -1,3 +1,5 @@
+package test;
+
 import option.MutableOption;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
--- src/test/OptionTest.java
+++ src/test/OptionTest.java
@@ -1,3 +1,5 @@
+package test;
+
 import option.Option;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
